b2f0218ca5f081fbd7b6a79f5693d7480ccdfc0f
ptrbui <peterbui@usc.edu>
2024-05-25 14:24:37 -0400

    initial commit
    32min

---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="image">
+                <img src="https://techcrunch.com/wp-content/uploads/2022/01/GettyImages-1208882806-1.jpg?resize=1536,1018" alt=""/>
+            </div>
+            <div className="texts">
+                <h2>AI tutors are quietly changing how kids in the US study, and the leading apps are from China</h2>
+                <p className="info">
+                    <a href="" className="author">Peter Bui</a>
+                    <time>2024-05-04 16:45</time>
+                </p>
+                <p className="summary">Evan, a high school sophomore from Houston, was stuck on a calculus problem. He pulled up Answer AI on his iPhone, snapped a photo of the problem from his Advanced Placement math textbook, and ran it through the homework app.</p>
+            </div>
+        </div>
+    );
+}
