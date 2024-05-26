7d82b1b3c4d2859528efdb0f6de96d5ba577c422
ptrbui <peterbui@usc.edu>
2024-05-26 15:36:49 -0400

    commint 3 day 2
    1:22:00

--- api/index.js
+++ api/index.js
@@ -2,17 +2,45 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const app = express();
+const jwt = require('jsonwebtoken');
 
-app.use(cors());
+const salt = bcrypt.genSaltSync(10);
+const secret = 'askihcboecbu973289gd3908dg2';
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://pbui2021:P36qut6r7lZplFEU@cluster0.ve2m99d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 app.post('/register', async (req,res) => {
     const{username, password} = req.body;
-    const userDoc = await User.create({username, password});
-    res.json(userDoc);
+    try {
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+        });
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e);
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        // logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+        // res.json();
+    } else {
+        res.status(400).json('wrong credentials');
+    }
 });
 
 app.listen(4000);
