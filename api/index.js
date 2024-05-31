962870cbd1b4e34f8a20ae94a2635949681438ca
ptrbui <peterbui@usc.edu>
2024-05-31 16:56:09 -0400

    w2.0
    2:13:43

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
@@ -63,13 +64,22 @@ app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
 })
 
-app.post('/post', uploadMiddleware.single('file'), (req,res) => {
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
     const {originalname, path} = req.file;
     const parts = originalname.split('.');
     const ext = parts[parts.length - 1];
     const newPath = path + '.' + ext;
     fs.renameSync(path, newPath);
-    res.json({ext});
+
+    const {title, summary, content} = req.body;
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath,
+    })
+
+    res.json(postDoc);
 });
 
 app.listen(4000);
