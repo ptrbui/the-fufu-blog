0b5616db31249916de76a2ec5a4a999d2caec61c
ptrbui <peterbui@usc.edu>
2024-06-01 16:45:52 -0400

    2:48:20

--- api/index.js
+++ api/index.js
@@ -96,6 +96,12 @@ app.get('/post', async (req, res) => {
     );
 });
 
+app.get('/post/:id', async (req, res) => {
+    const {id} = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+})
+
 app.listen(4000);
 
 // Cluster0
