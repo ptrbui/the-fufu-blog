06406dde1ea86e1acf544fd82db361bb32deb7ad
ptrbui <peterbui@usc.edu>
2024-06-05 19:29:54 -0400

    pusherrr
    3.08.21

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,5 +1,5 @@
 import {useContext, useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {formatISO9075} from "date-fns";
 import {UserContext} from "../UserContext";
 
@@ -25,7 +25,12 @@ export default function PostPage() {
             <div className="author">by @{postInfo.author.username}</div>
             {userInfo.id === postInfo.author._id && (
                 <div className="edit-row">
-                    <a className="edit-button" href="">Edit this post</a>
+                    <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                        </svg>
+                        Edit this post
+                    </Link>
                 </div>
             )}
             <div className="image">
