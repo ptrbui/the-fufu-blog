e23d5c388d1c2952e6ceefe380d5e445084faff1
ptrbui <peterbui@usc.edu>
2024-06-08 22:19:08 -0400

    testing

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const secret = process.env.SECRET_KEY;
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'https://fufu-blog.netlify.app/'] /*update with new url*/
+    origin: ['http://localhost:3000', 'https://fufu-blog.netlify.app'] /*update with new url*/
 }));
 app.use(express.json());
 app.use(cookieParser());
